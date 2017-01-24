853739337e9ee1cf9b5fdec931c65e52ef667149
Steve McKnight <stephen.mcknight@gmail.com>
2017-01-23 20:38:12 -0500

    Add web project for visualization

---
+++ FreezerMonitor.Web/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FreezerMonitor.Data;
+using System.Net;
+using System.Net.Mime;
+
+namespace FreezerMonitor.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var context = new FreezerContext("Data Source=C:\\temp\\freezer_temps.sqlite"))
+            {
+                var startDate = DateTime.Now.AddDays(-2);
+                var sensors = context.TemperatureReadings
+                    .Where(r => r.Timestamp >= startDate)
+                    .OrderBy(r => r.SensorID)
+                    .ThenBy(r => r.Timestamp)
+                    .GroupBy(r => r.SensorID)
+                    .ToList();
+                return View(new Models.SensorReadingsViewModel { Sensors = sensors });
+            }
+        }
+
+        
+        public ActionResult Log(String id)
+        {
+            try
+            {
+                var f = File(String.Format("~/{0}",id), MediaTypeNames.Text.Plain);
+                return f;
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FreezerMonitor.Web/Models/SensorReadingsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FreezerMonitor.Data;
+
+namespace FreezerMonitor.Web.Models
+{
+    public class SensorReadingsViewModel
+    {
+        public IList<IGrouping<String, TemperatureReading>> Sensors { get; set; }
+    }
+}
