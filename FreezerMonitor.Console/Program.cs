4ac1436d75fcbed04b104ca4635d903de2261b6f
Steve McKnight <stephen.mcknight@gmail.com>
2017-03-18 13:51:40 -0400

    Changed included timeframe and time format used for output

--- FreezerMonitor.Console/Program.cs
+++ FreezerMonitor.Console/Program.cs
@@ -25,10 +25,10 @@ namespace FreezerMonitor.TestConsole
 
                     Console.WriteLine("Sensor {0}", sensor.Key);
                     File.AppendAllText(f, "timestamp,temperature\n");
-                    foreach (var reading in sensor.Where(r => r.Timestamp > DateTime.Today.AddDays(-2)))
+                    foreach (var reading in sensor.Where(r => r.Timestamp > DateTime.Today.AddDays(-4)))
                     {
                         //Console.WriteLine("   Temp: {0}  --  {1:F2}", reading.Timestamp.ToLocalTime(), reading.Temperature);
-                        File.AppendAllText(f, String.Format("{0},{1}\n", reading.Timestamp.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffK"), reading.Temperature));
+                        File.AppendAllText(f, String.Format("{0},{1}\n", reading.Timestamp.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ"), reading.Temperature));
                     }
                     Console.WriteLine("  Records written to: {0}", f);
                 }
