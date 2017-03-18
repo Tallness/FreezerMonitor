fc31519012d937562a178c065b352b291f0b7b5b
Steve McKnight <stephen.mcknight@gmail.com>
2017-03-18 17:27:12 -0400

    Setup project for loading Sqlite to main data source

---
+++ FreezerMonitor.DataLoader/Program.cs
@@ -0,0 +1,25 @@
+﻿using FreezerMonitor.SqliteData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreezerMonitor.DataLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var context = new SqliteFreezerContext("Data Source=C:\\temp\\freezer_temps.sqlite"))
+            {
+                var sensors = context.TemperatureReadings
+                    .OrderByDescending(r => r.Timestamp)
+                    //.Take(40)
+                    .GroupBy(r => r.SensorID);
+            }
+
+            }
+        }
+}
