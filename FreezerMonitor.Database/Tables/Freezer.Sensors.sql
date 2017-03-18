0f76d772e2febeac57c1f38ba37c6a7145f2f398
Steve McKnight <stephen.mcknight@gmail.com>
2017-03-18 12:14:42 -0400

    Correct column length

--- FreezerMonitor.Database/Tables/Freezer.Sensors.sql
+++ FreezerMonitor.Database/Tables/Freezer.Sensors.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Freezer].[Sensors]
 (
 	[ID]           INT          IDENTITY NOT NULL,
-	[SensorNumber] NVARCHAR(10)          NOT NULL,
+	[SensorNumber] NVARCHAR(12)          NOT NULL,
 	[Description]  NVARCHAR(25)          NOT NULL,
 
 	CONSTRAINT PK_Sensors_ID PRIMARY KEY CLUSTERED (ID)
