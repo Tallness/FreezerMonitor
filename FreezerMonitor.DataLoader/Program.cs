e0fd5ef286078c78ecc5b839b67f166501c608d6
Steve McKnight <stephen.mcknight@gmail.com>
2017-03-18 21:47:39 -0400

    Implement loader for Sqlite raw data to SQL

--- FreezerMonitor.DataLoader/Program.cs
+++ FreezerMonitor.DataLoader/Program.cs
@@ -1,4 +1,5 @@
-﻿using FreezerMonitor.SqliteData;
+﻿using FreezerMonitor.Data;
+using FreezerMonitor.SqliteData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,42 @@ namespace FreezerMonitor.DataLoader
     {
         static void Main(string[] args)
         {
-
-            using (var context = new SqliteFreezerContext("Data Source=C:\\temp\\freezer_temps.sqlite"))
+            using (var context = new FreezerContext())
             {
-                var sensors = context.TemperatureReadings
-                    .OrderByDescending(r => r.Timestamp)
-                    //.Take(40)
-                    .GroupBy(r => r.SensorID);
+                var lastReadingDate = context.Readings.Max(r => r.Time);
+
+                using (var sqliteContext = new SqliteFreezerContext("Data Source=C:\\temp\\freezer_temps.sqlite"))
+                {
+                    var rawReadings = sqliteContext.TemperatureReadings
+                        .Where(r => r.Timestamp > lastReadingDate)
+                        .OrderBy(r => r.Timestamp);
+                    Console.WriteLine("{0} records found to import.", rawReadings.Count());
+
+                    var sensors = context.Sensors.ToDictionary(s => s.SensorNumber, s => s.ID);
+                    var recordCount = 0;
+                    var writtenRecordCount = 0;
+                    foreach (var rawReading in rawReadings)
+                    {
+                        context.Readings.Add(new Data.Entities.Reading()
+                        {
+                            SensorID = sensors[rawReading.SensorID],
+                            Time = rawReading.Timestamp.ToUniversalTime(),
+                            Temperature = rawReading.Temperature
+                        });
+                        recordCount++;
+
+                        if (recordCount >=100)
+                        {
+                            context.SaveChanges();
+                            writtenRecordCount += recordCount;
+                            Console.WriteLine("{0} total records written.", writtenRecordCount);
+                            recordCount = 0;
+                        }
+                    }
+                    context.SaveChanges();
+                }
             }
 
-            }
         }
+    }
 }
