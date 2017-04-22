a6c757a2a4e348211ebb31cd7b71a47acf74480a
Steve McKnight <stephen.mcknight@gmail.com>
2017-04-22 14:37:42 -0400

    Update python script for more robust logging

--- FreezerMonitor.Python/freezer_temp3.py
+++ FreezerMonitor.Python/freezer_temp3.py
@@ -1,14 +1,45 @@
+"""Script reads the temperature from each available sensor,
+and logs to a local sqlite database
+"""
 import sqlite3
+from datetime import datetime
 
+import requests
 from w1thermsensor import W1ThermSensor
 
-conn = sqlite3.connect('/home/pi/Freezer/freezer_temps.db')
-curs = conn.cursor()
+BASE_URL = "http://localhost:10108/"
 
-sensors = W1ThermSensor.get_available_sensors()
-for i, sensor in enumerate(sensors):
-    #print("%s %s %.4f" % (datetime.now().isoformat(), sensor.id, sensor.get_temperature(W1ThermSensor.DEGREES_F)))
-    #log.write("%s %s %.4f\n" % (datetime.now().isoformat(), sensor.id, sensor.get_temperature(W1ThermSensor.DEGREES_F)))
-    curs.execute("""INSERT INTO temperatures values(datetime('now'),(?),(?))""",
-                 (sensor.id, sensor.get_temperature(W1ThermSensor.DEGREES_F)))
+
+def main():
+    """ Main entry point"""
+    read_time = datetime.utcnow().isoformat()
+    # status = log_reading("03168a1eedff", datetime.utcnow().isoformat(), 100)
+    # print status
+    sensors = W1ThermSensor.get_available_sensors()
+
+    for dummy_i, sensor in enumerate(sensors):
+        temperature = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+        status = log_reading(sensor.id, read_time, temperature)
+        log_to_sqlite(sensor.sensor_id, read_time, temperature)
+        print status
+
+
+def log_reading(sensor_id, time, temperature):
+    """Publish a temperature reading to web service"""
+    url = BASE_URL + "readings/" + sensor_id
+    payload = {'time': time, 'temperature': temperature}
+    response = requests.put(url, json=payload)
+    return response.status_code
+
+
+def log_to_sqlite(sensor_id, time, temperature):
+    """Log temp reading to local storage"""
+    conn = sqlite3.connect('/home/pi/Freezer/freezer_temps.db')
+    curs = conn.cursor()
+    curs.execute("""INSERT INTO temperatures values((?),(?),(?))""",
+                 (time, sensor_id, temperature))
     conn.commit()
+
+
+if __name__ == "__main__":
+    main()
